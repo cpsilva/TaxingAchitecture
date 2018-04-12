9bc2ba46256889fadafeeecff7da3e5110eee59c
Caio Pires Silva <caio.pires@ctis.com.br>
2018-04-12 09:09:30 -0300

    Adicionado projetos e configurado a injeção de dependencia na aplicação

---
+++ TaxingArchitecture/Tax.ApplicationService/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Tax.DependenceInjection;
+
+namespace Tax.ApplicationService.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddDependencyInjectionCustom(this IServiceCollection services, IConfiguration configuration)
+        {
+            Container.RegisterServices(services);
+        }
+    }
+}
---
+++ TaxingArchitecture/Tax.ViewModels/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Tax.ViewModels
+{
+    public class Class1
+    {
+    }
+}
