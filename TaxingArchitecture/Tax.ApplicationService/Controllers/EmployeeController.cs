23cf7f3ad37bd9ec097ea8f62daa68fbd2d79c87
cpsilva <caiopirees@gmail.com>
2018-04-12 14:53:48 -0300

    Correção do repositorio

--- TaxingArchitecture/Tax.ApplicationService/Controllers/EmployeeController.cs
+++ TaxingArchitecture/Tax.ApplicationService/Controllers/EmployeeController.cs
@@ -6,7 +6,6 @@ namespace Tax.ApplicationService.Controllers
     [Route("[controller]")]
     public class EmployeeController : Controller
     {
-        // GET
         public EmployeeViewModel Get(EmployeeViewModel employee)
         {
             return new EmployeeViewModel { Nome = "teste" };
